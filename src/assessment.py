26da46706daf5086f956b3e35ce5a12f6d1abd90
Nick <choiyoonjaenicholas@gmail.com>
2019-01-29 18:53:31 -0600

    Nick assessment1

--- src/assessment.py
+++ src/assessment.py
@@ -3,15 +3,15 @@ Write your code under the commented(#) question. Replace the pass with your code
 '''
 
 ### Question1
-### Print 2**0 + 2**1 + 2**2
+### Print (2**0 + 2**1 + 2**2)
 
-pass
-
+Print (2**0 + 2**1 + 2**2)
+Print ('hello')
 
 
 ### Question2
 ### Calculate the sum of list A and print it
 
-A = [17, 14, 14, 14, 16, 15, 16, 14, 15, 13, 18, 13]
+A = [17, 14, 14, 14, 16, 15, 16, 14, 15, 13, 'message')
 
-pass
+sum (A)
